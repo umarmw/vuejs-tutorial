d5ccbabc4a17a1c3786fb76edc7c7ce6ac5b100b
Umar Waliyullah <umar.mw@gmail.com>
2016-05-16 13:56:25 +0400

    update home page

---
+++ src/components/TimeEntries.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <button
+      v-if="$route.path !== '/time-entries/log-time'"
+      v-link="'/time-entries/log-time'"
+      class="btn btn-primary">
+      Log Time
+    </button>
+
+    <div v-if="$route.path === '/time-entries/log-time'">
+      <h3>Log Time</h3>
+    </div>
+
+    <hr>
+
+    <router-view></router-view>
+
+    <div class="time-entries">
+      <p v-if="!timeEntries.length"><strong>No time entries yet</strong></p>
+
+      <div class="list-group">
+
+        <a class="list-group-item" v-for="timeEntry in timeEntries">
+          <div class="row">
+
+            <div class="col-sm-2 user-details">
+              <img :src="timeEntry.user.image" class="avatar img-circle img-responsive" />
+              <p class="text-center">
+                <strong>
+                  {{ timeEntry.user.firstName }}
+                  {{ timeEntry.user.lastName }}
+                </strong>
+              </p>
+            </div>
+
+            <div class="col-sm-2 text-center time-block">
+              <h3 class="list-group-item-text total-time">
+                <i class="glyphicon glyphicon-time"></i>
+                {{ timeEntry.totalTime }}
+              </h3>
+              <p class="label label-primary text-center">
+                <i class="glyphicon glyphicon-calendar"></i>
+                {{ timeEntry.date }}
+              </p>
+            </div>
+
+            <div class="col-sm-7 comment-section">
+              <p>{{ timeEntry.comment }}</p>
+            </div>
+
+            <div class="col-sm-1">
+              <button
+                class="btn btn-xs btn-danger delete-button"
+                @click="deleteTimeEntry(timeEntry)">
+                X
+              </button>
+            </div>
+
+          </div>
+        </a>
+
+      </div>
+    </div>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
+import Home from './components/Home.vue'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+// We want to apply VueResource and VueRouter
+// to our Vue instance
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter()
+
+// Pointing routes to the components they should use
+router.map({
+  '/hello': {
+    component: Home
+  }
 })
+
+// Any invalid route will redirect to home
+router.redirect({
+  '*': '/hello'
+})
+
+router.start(App, '#app')
